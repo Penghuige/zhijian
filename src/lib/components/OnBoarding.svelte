8efdc476df478ec65d4832bddf1f59832c284d7a
penghui <2249365539@qq.com>
2026-06-04 22:10:54 +0800

    Initial commit of open-webui project

---
+++ src/lib/components/OnBoarding.svelte
@@ -0,0 +1,64 @@
+<script>
+	import { getContext, onMount } from 'svelte';
+	const i18n = getContext('i18n');
+
+	import Marquee from './common/Marquee.svelte';
+	import SlideShow from './common/SlideShow.svelte';
+	import ArrowRightCircle from './icons/ArrowRightCircle.svelte';
+
+	export let show = true;
+	export let getStartedHandler = () => {};
+</script>
+
+{#if show}
+	<div class="w-full h-screen max-h-[100dvh] text-white relative">
+		<SlideShow duration={5000} />
+
+		<div
+			class="w-full h-full absolute top-0 left-0 bg-linear-to-t from-20% from-black to-transparent"
+		></div>
+
+		<div class="w-full h-full absolute top-0 left-0 backdrop-blur-xs bg-black/50"></div>
+
+		<div class="relative bg-transparent w-full h-screen max-h-[100dvh] flex z-10">
+			<div class="flex flex-col justify-end w-full items-center pb-10 text-center">
+				<div class="text-5xl lg:text-7xl font-secondary">
+					<Marquee
+						duration={5000}
+						words={[
+							$i18n.t('Explore the cosmos'),
+							$i18n.t('Unlock mysteries'),
+							$i18n.t('Chart new frontiers'),
+							$i18n.t('Dive into knowledge'),
+							$i18n.t('Discover wonders'),
+							$i18n.t('Ignite curiosity'),
+							$i18n.t('Forge new paths'),
+							$i18n.t('Unravel secrets'),
+							$i18n.t('Pioneer insights'),
+							$i18n.t('Embark on adventures')
+						]}
+					/>
+
+					<div class="mt-0.5">{$i18n.t(`wherever you are`)}</div>
+				</div>
+
+				<div class="flex justify-center mt-8">
+					<div class="flex flex-col justify-center items-center">
+						<button
+							aria-label={$i18n.t('Get started')}
+							class="relative z-20 flex p-1 rounded-full bg-white/5 hover:bg-white/10 transition font-medium text-sm"
+							on:click={() => {
+								getStartedHandler();
+							}}
+						>
+							<ArrowRightCircle className="size-6" aria-hidden="true" />
+						</button>
+						<div class="mt-1.5 font-primary text-base font-medium" aria-hidden="true">
+							{$i18n.t(`Get started`)}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+{/if}
---
+++ src/lib/components/app/AppSidebar.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	import Tooltip from '$lib/components/common/Tooltip.svelte';
+
+	let selected = '';
+</script>
+
+<nav
+	aria-label="App navigation"
+	class="min-w-[4.5rem] bg-gray-50 dark:bg-gray-950 flex gap-2.5 flex-col pt-8"
+>
+	<div class="flex justify-center relative">
+		{#if selected === 'home'}
+			<div class="absolute top-0 left-0 flex h-full">
+				<div class="my-auto rounded-r-lg w-1 h-8 bg-black dark:bg-white"></div>
+			</div>
+		{/if}
+
+		<Tooltip content="Home" placement="right">
+			<button
+				aria-label="Home"
+				class=" cursor-pointer {selected === 'home' ? 'rounded-2xl' : 'rounded-full'}"
+				on:click={() => {
+					selected = 'home';
+
+					if (window.electronAPI) {
+						window.electronAPI.load('home');
+					}
+				}}
+			>
+				<span class="flex size-11 items-center justify-center text-sm font-semibold">职</span>
+			</button>
+		</Tooltip>
+	</div>
+
+	<div class=" -mt-1 border-[1.5px] border-gray-100 dark:border-gray-900 mx-4"></div>
+
+	<div class="flex justify-center relative group">
+		{#if selected === ''}
+			<div class="absolute top-0 left-0 flex h-full">
+				<div class="my-auto rounded-r-lg w-1 h-8 bg-black dark:bg-white"></div>
+			</div>
+		{/if}
+		<button
+			aria-label="Chat"
+			class=" cursor-pointer bg-transparent"
+			on:click={() => {
+				selected = '';
+			}}
+		>
+			<span
+				class="flex size-10 items-center justify-center text-sm font-semibold {selected === ''
+					? 'rounded-2xl'
+					: 'rounded-full'}"
+				>问</span
+			>
+		</button>
+	</div>
+
+	<!-- <div class="flex justify-center relative group text-gray-400">
+		<button class=" cursor-pointer p-2" on:click={() => {}}>
+			<Plus className="size-4" strokeWidth="2" />
+		</button>
+	</div> -->
+</nav>
